79f04ce7101416f23f2eb4f6ccfea3236f732be5
Daniel Heim <danielheim@gmail.com>
2023-06-29 20:57:43 +1000

    Installed React.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>It may not look like much, but it's got it where it counts, kid.</p>
+    </div>
+  );
+}
+
+export default App;
