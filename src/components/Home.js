16f8990dfc225f01f31148028d1373484557973f
Daniel Heim <danielheim@gmail.com>
2023-07-17 20:20:42 +1000

    Content refactor.

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,86 +17,80 @@ function Desc(props) {
 	</div>
 }
 
+function Item(props) {
+	return <article className={props.blockName} key={props.id} id={props.id}>
+		{ props.img && <img
+			className={`${props.blockName}__img`}
+			src={props.img}
+			alt=''
+		/> }
+		<div>
+			<header>
+				<h1>{props.title}</h1>
+			</header>
+			<Desc desc={props.desc} />
+		</div>
+	</article>
+}
+
 function Content(props) {
 
-	function Item(props) {
-		return <article className={props.blockName} key={props.id} id={props.id}>
-			{ props.img && <img
-				className={`${props.blockName}__img`}
-				src={props.img}
-				alt=''
-			/> }
-			<div>
-				<header>
-					<h1>{props.title}</h1>
-				</header>
-				<Desc desc={props.desc} />
-			</div>
-		</article>
-	}
-
-	return <section id={props.id} className={props.blockName}>
+	return <div id={props.id} className={props.blockName}>
 		<header>
 			<h1 className={`${props.blockName}__title`}>{props.title}</h1>
+			{ props.subtitle && <p className={`${props.blockName}__subtitle`}>{props.subtitle}</p> }
 		</header>
 		<Desc desc={props.desc} thin={true} />
-		{ props.items?.length > 0 && <div className={`${props.blockName}__articles`}>
-			{props.items?.map(i => <Item
-				key={i.id}
-				blockName={`${props.blockName}_article`}
-				{...i}
-			/>)}
-		</div> }
-	</section>
+		{ props.children }
+	</div>
 }
 
 function Home() {
 
 	const {
-		home,
+		intro,
 		about,
 		services
 	} = useLoaderData();
 
-	const parseSocial = (social) => {
+	const Social = (props) => {
 
-		const svg = (() => { switch (social.id) {
+		const svg = (() => { switch (props.id) {
 			case 'github': return <GithubSvg />;
 			case 'linkedin': return <LinkedinSvg />;
 			case 'twitter': return <TwitterSvg />;
 			default: return null;
 		}})();
 
-		return <li key={social.id} id={social.id}>
+		return <li key={props.id} id={props.id}>
 			<a
-				href={social.href}
+				href={props.href}
 				target='_blank'
 				rel='noopener noreferrer'>
-				{/* <img src={imgSrc} role='presentation' /> */}
 				{svg}
 			</a>
 		</li>
 	}
 
 	return <>
-		<div className='section'>
-			<div className='home'>
-				<header>
-					<h1 className='home__title'>{home.title}</h1>
-					<p className='home__subtitle'>{home.subtitle}</p>
-				</header>
-				<Desc desc={home.desc} thin={true} />
-				<ul className='home__socials'>
-					{home.socials.map(parseSocial)}
+		<section className='section'>
+			<Content blockName='intro' {...intro} >
+				<ul className='intro__socials'>
+					{intro.socials.map(v => <Social {...v} />)}
 				</ul>
-			</div>
-		</div>
-		<div className='section'>
-			<Content blockName='about' {...about} />
-		</div>
-		<div className='section'>
-			<Content blockName='services' {...services} />
-		</div>
+			</Content>
+		</section>
+		<section className='section'>
+			<Content blockName='about' {...about}>
+			</Content>
+		</section>
+		<section className='section'>
+			<Content blockName='services' {...services}>
+				<div className='services__articles'>
+				{services.items.map(v => <Item blockName='services_article' {...v} />)}
+				</div>
+			</Content>
+		</section>
 	</>
 }
 
